7fd4be81809bcbc397bae5105ac83e9fd0816206
diegomagrini <me@diegomagrini.com>
2023-05-10 14:40:03 +0100

    Added extraction for all relevant ICAO codes.

--- FPL_Extraction/main.py
+++ FPL_Extraction/main.py
@@ -4,6 +4,7 @@ import re
 # Regular expressions used to extract the data
 findFplMessage = '\(FPL[A-Za-z0-9\-\s\/]*\)'
 findFplFields = '\-[A-Za-z0-9\/\s]*'
+findIcaoCode = '[A-Za-z]{4}'
 
 
 # Isolate and broadly validate the input FPL message
@@ -14,7 +15,7 @@ def validate_fpl(path):
         if fpl:
             message = fpl.group()
         else:
-            return 'Invalid FPL message'
+            exit('Invalid FPL message')
 
         # remove all `\n` characters
         message = message.replace('\n', '')
@@ -25,11 +26,52 @@ def validate_fpl(path):
 # Extract the fields from the FPL message
 def extract_fpl_fields(fpl):
     fpl_fields = re.findall(findFplFields, fpl)
-    return fpl_fields
+
+    # Create the flightplan object
+    flightplan = {}
+
+    # capture the departure airport from field 13
+    flightplan['adep'] = re.search(findIcaoCode, fpl_fields[4]).group()
+
+    # extract field 16
+    field_16 = fpl_fields[6]
+    # extract the destination airport from field 16
+    flightplan['ades'] = re.search('([A-Za-z]{4})[0-9]{4}', field_16).group(1)
+    # extract all alternate airports from field 16
+    flightplan['dalts'] = re.findall(findIcaoCode, field_16)[1:]
+
+    # extract field 18
+    field_18 = fpl_fields[7]
+
+    # find the EET section in field 18
+    eet = re.search('EET\/([A-Za-z0-9\s]*)\s{1}[A-Za-z]+\/', field_18).group(1)
+    # extract all FIR codes
+    firs = re.findall(findIcaoCode, eet)
+    # remove duplicates and assign to flightplan
+    flightplan['firs'] = list(dict.fromkeys(firs))
+
+    # find the RALT section in field 18
+    ralt = re.search('RALT\/([A-Za-z0-9\s]*)\s{1}[A-Za-z]+\/', field_18)
+    if ralt:
+        ralt = ralt.group(1)
+        # extract all enroute alternates
+        flightplan['ralts'] = re.findall(findIcaoCode, ralt)
+    else:
+        flightplan['ralts'] = []
+
+    #find the TALT section in field 18
+    talt = re.search('TALT\/([A-Za-z0-9\s]*)\s{1}[A-Za-z]+\/', field_18)
+    if talt:
+        talt = talt.group(1)
+        # extract the takeoff alternate
+        flightplan['talt'] = re.search(findIcaoCode, talt).group()
+    else:
+        flightplan['talt'] = ''
+
+    return flightplan
 
 
 # if the script is running from the main directory, execute the extract function
 if __name__ == "__main__":
     fpl = validate_fpl('Sample_Data/5.txt')
-    print(fpl)
     print(extract_fpl_fields(fpl))
